f310f522697dc9eef042cab694aabb6183992c9d
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-05 19:44:51 +0200

    Update welcome message for clarity.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the remote work platform! Enjoy your stay.');
+  res.send('Welcome to our remote work platform! We hope you enjoy your experience.');
 });
 
 app.listen(PORT, () => {
