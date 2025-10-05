52218d6cd9d47df4dff302140508c1006c7ba892
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-05 19:43:55 +0200

    Fix typo in the welcome message.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the remote work platform!');
+  res.send('Welcome to the remote work platform! Enjoy your stay.');
 });
 
 app.listen(PORT, () => {
